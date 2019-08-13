03370e721237411a83618bc72ac6c711aec6cb5b
Hcg101 <hcg101@gmail.com>
2019-08-13 18:21:46 +0800

    addwifdog-compile-firmware

---
+++ trunk/libs/libwebsockets/libwebsockets-2.0.3/CMakeFiles/test-server.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/opt/rt-n56u/trunk/libs/libwebsockets/libwebsockets-2.0.3/test-server/test-server-dumb-increment.c" "/opt/rt-n56u/trunk/libs/libwebsockets/libwebsockets-2.0.3/CMakeFiles/test-server.dir/test-server/test-server-dumb-increment.c.o"
+  "/opt/rt-n56u/trunk/libs/libwebsockets/libwebsockets-2.0.3/test-server/test-server-echogen.c" "/opt/rt-n56u/trunk/libs/libwebsockets/libwebsockets-2.0.3/CMakeFiles/test-server.dir/test-server/test-server-echogen.c.o"
+  "/opt/rt-n56u/trunk/libs/libwebsockets/libwebsockets-2.0.3/test-server/test-server-http.c" "/opt/rt-n56u/trunk/libs/libwebsockets/libwebsockets-2.0.3/CMakeFiles/test-server.dir/test-server/test-server-http.c.o"
+  "/opt/rt-n56u/trunk/libs/libwebsockets/libwebsockets-2.0.3/test-server/test-server-mirror.c" "/opt/rt-n56u/trunk/libs/libwebsockets/libwebsockets-2.0.3/CMakeFiles/test-server.dir/test-server/test-server-mirror.c.o"
+  "/opt/rt-n56u/trunk/libs/libwebsockets/libwebsockets-2.0.3/test-server/test-server-status.c" "/opt/rt-n56u/trunk/libs/libwebsockets/libwebsockets-2.0.3/CMakeFiles/test-server.dir/test-server/test-server-status.c.o"
+  "/opt/rt-n56u/trunk/libs/libwebsockets/libwebsockets-2.0.3/test-server/test-server.c" "/opt/rt-n56u/trunk/libs/libwebsockets/libwebsockets-2.0.3/CMakeFiles/test-server.dir/test-server/test-server.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS
+  "INSTALL_DATADIR=\"/opt/rt-n56u/trunk/stage/share\""
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/opt/rt-n56u/trunk/libs/libwebsockets/libwebsockets-2.0.3/CMakeFiles/websockets.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  "lib"
+  "/opt/rt-n56u/trunk/stage/include"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
